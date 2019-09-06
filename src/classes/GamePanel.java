a193c1fa6ee7a83735f349f2baba5ba16d16a60f
Doron <dodo.golan@gmail.com>
2019-09-06 13:00:29 +0300

    commit

--- src/classes/GamePanel.java
+++ src/classes/GamePanel.java
@@ -1,10 +1,68 @@
 package classes;
 
-public class GamePanel {
+import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
+import javax.swing.*;
+
+public class GamePanel extends JPanel {
+	
+	// main game frame
+	private JFrame mainFrame;
+	
+	// background image
+	private Image background;
+	
+	// all the objects that take part of this   
+	private Player player;
+	
+	/**
+	 * GamePanel constructor
+	 * 
+	 */
+	public GamePanel() {
+		ImageIcon ii = new ImageIcon("/images/main_background");
+		this.background = ii.getImage();
+		this.player = new Player(this);
+		addKeyListener(new KL());
+		
+	}
+	
+	private class KL extends KeyAdapter{
+		
+		public void KeyPressed(KeyEvent e) {
+			int code = e.getKeyCode();
+			
+			switch(code) {
+			case KeyEvent.VK_UP:
+				player.updateY(-20);
+				break;
+			case KeyEvent.VK_DOWN:
+				player.updateY(20);
+				break;
+			case KeyEvent.VK_LEFT:
+				player.updateX(-20);
+				break;
+			case KeyEvent.VK_RIGHT:
+				player.updateX(20);
+				break;
+			default:
+				player.updateX(0);
+				break;
+			}
+		}
+	}
+	
+	/**
+	 * main function
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 	}
+	
+	
 
 }
--- src/classes/Player.java
+++ src/classes/Player.java
@@ -1,5 +1,82 @@
 package classes;
 
-public class Player {
+import java.awt.*;
 
+import javax.swing.*;
+
+public class Player implements Runnable {
+
+	// the game panel
+	private GamePanel gp;
+	// the player coordinates
+	private int x;
+	private int y;
+	
+	// player appearance
+	private Image playerLook;
+	
+	// TODO: add array of items for buffs and debuffs
+	
+	// player strength, might and will be effected during game 
+	private double power;
+	
+	// player health points
+	private int hp;
+	
+	// how many tries the player has until game over
+	private int lives;
+	
+	/**
+	 * Player class constructor
+	 */
+	public Player(GamePanel gp) {
+		this.gp = gp;
+		this.x = 500;
+		this.y = 1600;
+		this.power = 1;
+		this.lives = 3;
+		this.hp = 200;
+		ImageIcon ii = new ImageIcon("/images/base_player");
+		this.playerLook = ii.getImage();
+	}
+
+	/**
+	 * get player x coordinate
+	 * @return players x value
+	 */
+	public int getX() {
+		return this.x;
+	}
+	
+	/**
+	 * get player y coordinate
+	 * @return players y value
+	 */
+	public int getY() {
+		return this.y;
+	}
+	
+	/**
+	 * Update player x coordinate
+	 * @param add To move Left => add: negative value;   
+	 * 			    To move right => add: positive value;  
+	 */
+	public void updateX(int add) {
+		this.x += add;
+	}
+	/**
+	 * Update player y coordinate
+	 * @param add To move up => add: negative value;   
+	 * 			    To move down => add: positive value;  
+	 */
+	public void updateY(int add) {
+		this.y += add;
+	}
+	
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+	
 }
